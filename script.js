a5fd3ebad03020535d9f1e21eb84ce0ecae61d7c
Jesse Ceja <jvc68@nau.edu>
2025-02-21 18:05:37 -0700

    Created basic button functionalty
    The button now reacts when clicked, not visually but internally

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hey bruh");
+let seeds = 0;
+let clickPower = 1;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+
+function createSeedButton() {
+    document.getElementById("seed").addEventListener("click", () => addSeed());
+}
+
+function initializeData() {
+    createSeedButton();
+    if (localStorage.getItem("clickPower") == null) {
+        localStorage.setItem("clickPower", 1);
+        localStorage.setItem("seeds", 0);
+    } else {
+        seeds = parseInt(localStorage.getItem("seeds"));
+        clickPower = parseInt(localStorage.getItem("clickPower"));
+        document.getElementById("total").textContent = `Seeds: ${seeds}`;
+
+    }
+}
+
+function addSeed() {
+
+    seeds = seeds + clickPower;
+
+    document.getElementById("total").textContent = `Seeds: ${seeds}`;
+}
+
+async function saveData() {
+    await delay(500);
+    localStorage.setItem("seeds", seeds);
+    localStorage.setItem("clickPower", clickPower);
+    saveData();
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    initializeData();
+    saveData();
+
+});
