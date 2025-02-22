b494336dd79ef5b19e76f72145b18767d59d6dc3
Jesse Ceja <jvc68@nau.edu>
2025-02-21 20:34:37 -0700

    Made the game like better
    Idk bruh I made it so that the "seeds" save to localStorage and added a new button that increases the amount of seeds you get per click.

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 let seeds = 0;
 let clickPower = 1;
+let clickPowerCost = 100;
 const delay = ms => new Promise(res => setTimeout(res, ms));
 
-function createSeedButton() {
+async function createButtons() {
     document.getElementById("seed").addEventListener("click", () => addSeed());
+    document.getElementById("clickPowa").addEventListener("click", () => upgradeClick());
+}
+
+function upgradeClick() {
+    let newCost = clickPowerCost;
+    let clickLvl = parseInt(localStorage.getItem("clickLevel"));
+    for (let i = 1; i < clickLvl; i++) {
+        newCost = newCost * 3.5;
+    }
+    if (newCost <= seeds) {
+        seeds = seeds - newCost;
+        localStorage.setItem("clickLevel", clickLvl + 1);
+        localStorage.setItem("clickPower", parseInt(localStorage.getItem("clickPower") * 2));
+        clickPower = parseInt(localStorage.getItem("clickPower"));
+        updateSeedCount();
+    }
 }
 
 function initializeData() {
-    createSeedButton();
-    if (localStorage.getItem("clickPower") == null) {
+    createButtons();
+    if (isNaN(parseInt(localStorage.getItem("clickPower")))) {
         localStorage.setItem("clickPower", 1);
         localStorage.setItem("seeds", 0);
+        localStorage.setItem("clickLevel", 1);
     } else {
         seeds = parseInt(localStorage.getItem("seeds"));
         clickPower = parseInt(localStorage.getItem("clickPower"));
@@ -20,14 +38,19 @@ function initializeData() {
 }
 
 function addSeed() {
-
     seeds = seeds + clickPower;
+    updateSeedCount();
 
-    document.getElementById("total").textContent = `Seeds: ${seeds}`;
+}
+
+let total = document.getElementById("total")
+
+async function updateSeedCount() {
+    total.textContent = `Seeds: ${seeds}`;
 }
 
 async function saveData() {
-    await delay(500);
+    await delay(1200);
     localStorage.setItem("seeds", seeds);
     localStorage.setItem("clickPower", clickPower);
     saveData();
